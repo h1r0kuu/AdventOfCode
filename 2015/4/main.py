2cf03d7b3134dc9abdcdca5e8329bf5080e01683
h1r0kuu <abhazim.nurbat@gmail.com>
2025-11-23 14:27:33 +0100

    2015/6

--- 2015/4/main.py
+++ 2015/4/main.py
@@ -1,2 +1,14 @@
+from hashlib import md5
+
 s = input()
-print(bytes.hex(s.encode()))
+i = 0
+res = None
+while True:
+    new_s = s + str(i)
+    hashed = md5(new_s.encode())
+    print(i)
+    if '000000' == hashed.hexdigest()[0:6]:
+        res = i
+        break
+    i += 1
+print(f"res {res}")
